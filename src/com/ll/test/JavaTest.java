0416d47549a3d658a5add35e89869444d17c0254
hll814.love@163.com <hll814.love@163.com>
2019-11-05 17:39:37 +0800

    上传第二个项目到github

---
+++ src/com/ll/test/JavaTest.java
@@ -0,0 +1,14 @@
+package com.ll.test;
+
+/**
+ * @author hll
+ * @version V1.0
+ * @ClassName:TestComment
+ * @Description:注釋模板
+ * @date 2019年9月5日 下午17:53
+ */
+public class JavaTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
