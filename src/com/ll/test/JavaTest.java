a22e81b98ba89e5424fa77e1a347063b5965d2c8
hll814.love@163.com <hll814.love@163.com>
2019-11-05 17:49:21 +0800

    修改JavaTest.java类

--- src/com/ll/test/JavaTest.java
+++ src/com/ll/test/JavaTest.java
@@ -11,5 +11,6 @@ public class JavaTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
